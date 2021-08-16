e352f007bc65763b394f57c7187b594063392c39
wangpeiqing <peiqing.wang@ishansong.com>
2021-08-16 20:10:53 +0800

    update

---
+++ list/list.go
@@ -0,0 +1,14 @@
+package list
+
+type List struct {
+	Perv *List
+	Val interface{}
+}
+
+func NewSingle() *List {
+
+}
+
+func NewTwoWay() *List {
+	
+}
--- queue/list.go
+++ queue/list.go
@@ -1,20 +1,20 @@
 package queue
 
-type List struct {
+type Queue struct {
 	elements []interface{}
 }
 
-func NewList() *List {
-	list := new(List)
+func NewQueue() *Queue {
+	list := new(Queue)
 	list.elements = make([]interface{}, 0)
 
 	return list
 }
 
-func (l *List) Push(element interface{}) {
+func (l *Queue) Push(element interface{}) {
 	l.elements = append(l.elements, element)
 }
-func (l *List) Pop() (int, interface{}) {
+func (l *Queue) Pop() (int, interface{}) {
 	if len(l.elements) > 0 {
 		element := l.elements[0]
 		l.elements = l.elements[1:]
@@ -25,9 +25,58 @@ func (l *List) Pop() (int, interface{}) {
 	return -1, nil
 }
 
-type CircularList struct {
-	Use int
-	Front int
-	Rear int
+type CircularQueue struct {
+	Used     int
+	Front    int
+	Rear     int
 	Capacity int
+	Members  []interface{}
+}
+
+func NewCircularQueue(cap int) *CircularQueue {
+	circalarQueue := new(CircularQueue)
+	circalarQueue.Capacity = cap
+	circalarQueue.Members = make([]interface{}, 0)
+
+	return circalarQueue
+}
+
+func (cq *CircularQueue) Push(member interface{}) bool {
+	if cq.IsFull() {
+		return false
+	}
+
+	cq.Used += 1
+
+	cq.Members = append(cq.Members, member)
+
+	return true
+}
+
+func (cq *CircularQueue) Pop() interface{} {
+	if cq.IsEmpty() {
+		return nil
+	}
+
+	cq.Used -= 1
+	member := cq.Members[0]
+	cq.Members = cq.Members[1:]
+
+	return member
+}
+
+func (cq *CircularQueue) IsFull() bool {
+	return cq.Used == cq.Capacity
+}
+
+func (cq *CircularQueue) IsEmpty() bool {
+	return cq.Used == 0
+}
+
+func (cq *CircularQueue) Next(i int) interface{} {
+	return cq.Members[(i+1)%cq.Capacity]
+}
+
+func (cq *CircularQueue) Last(i int) interface{} {
+	return cq.Members[(i-1+cq.Capacity)%cq.Capacity]
 }
